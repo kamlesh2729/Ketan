fd49f1492a59595fe5fa33869df7aaa4915bb9f5
Kamlesh <kamleshtankar96@gmail.com>
2024-07-16 02:25:09 +0530

    done

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
-let Nav = document.querySelector(['data-header']);
+let Nav = document.querySelector("[data-header]");
+let MenuOpen = document.querySelector("[data-MenuBtn]");
+let MenuClose = document.querySelector("[data-CloseBtn]");
+let MobNav = document.querySelector("[data-Mobnav]");
+
+const HandelScroll = () => {
+    if (window.scrollY > 0) {
+        Nav.style.position = "sticky";
+        Nav.style.top = "0"
+    } else {
+        Nav.style.position = "relative";
+    }
+}
+
+window.addEventListener('scroll', HandelScroll)
+
+
+
+MenuOpen.addEventListener("click", () => {
+MobNav.style.left = "0";
+});
+
+MenuClose.addEventListener("click", () => {
+MobNav.style.left = "-100%";
+});
+
+function MenuLink() {
+    MobNav.style.left = "-100%";
+}
